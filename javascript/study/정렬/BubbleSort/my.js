a196bc2b3145c7dce03577da62ce31486c1eb6bb
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-07-25 16:50:51 +0900

    BubbleSort

---
+++ javascript/study/정렬/BubbleSort/my.js
@@ -0,0 +1,12 @@
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+};
+
+console.log(bubbleSort([3, 1, 56, 7, 8, 10, -1]));
---
+++ javascript/study/정렬/BubbleSort/refactor.js
@@ -0,0 +1,17 @@
+// Optimized BubbleSort with noSwaps
+const bubbleSort = (array) => {
+  let noSwaps;
+  for (let i = array.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return array;
+};
+
+console.log(bubbleSort([3, 1, 56, 7, 8, 10, -1]));
