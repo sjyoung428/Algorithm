378443436eefacb4f05ce1ab7e1929e45386ab4a
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-07-25 15:13:05 +0900

    이진 검색 재귀

---
+++ javascript/study/검색/이진검색/재귀/my.js
@@ -0,0 +1,21 @@
+const binarySearch = (array, value) => {
+  let left = 0;
+  let right = array.length - 1;
+
+  const recursive = (start, end) => {
+    let middle = Math.floor((start + end) / 2);
+
+    if (array[middle] === value) return middle;
+
+    if (start > end) return -1;
+
+    if (array[middle] < value) {
+      return recursive(middle + 1, end);
+    } else if (array[middle] > value) {
+      return recursive(start, middle - 1);
+    }
+  };
+  return recursive(left, right);
+};
+
+console.log(binarySearch([1, 2, 4, 5, 7, 8, 9], 2));
